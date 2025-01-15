3b58481e754b4cf57b079457880f4f3b7a1503a6
AgSpades <86034982+AgSpades@users.noreply.github.com>
2025-01-16 00:21:55 +0530

    feat: generate and display private and public keys along with a P2PKH address

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using NBitcoin;
 
-Console.WriteLine("Hello World! "+ new Key().GetWif(Network.Main));
+Key privKey = new(); // Generate a random private key
+PubKey pubKey = privKey.PubKey; // Derive the public key from the private key
+
+Console.WriteLine($"Private key: {privKey.GetWif(Network.TestNet)}");
+Console.WriteLine($"Public key: {pubKey}");
+
+// We will use the key to create a P2PKH address
+var address = pubKey.GetAddress(ScriptPubKeyType.Legacy, Network.TestNet);
+Console.WriteLine($"Address: {address}");
