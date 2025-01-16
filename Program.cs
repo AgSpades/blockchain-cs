83b0610ae07fa8c964e9640f8342b3a9204d0219
AgSpades <86034982+AgSpades@users.noreply.github.com>
2025-01-16 12:50:15 +0530

    feat: update address generation to use main network and display private key

--- Program.cs
+++ Program.cs
@@ -3,9 +3,7 @@
 Key privKey = new(); // Generate a random private key
 PubKey pubKey = privKey.PubKey; // Derive the public key from the private key
 
-Console.WriteLine($"Private key: {privKey.GetWif(Network.TestNet)}");
-Console.WriteLine($"Public key: {pubKey}");
-
-// We will use the key to create a P2PKH address
-var address = pubKey.GetAddress(ScriptPubKeyType.Legacy, Network.TestNet);
+var address = pubKey.GetAddress(ScriptPubKeyType.Legacy, Network.Main);
 Console.WriteLine($"Address: {address}");
+Console.WriteLine($"Public key: {pubKey}");
+Console.WriteLine($"Private key: {privKey.GetWif(Network.Main)}");
